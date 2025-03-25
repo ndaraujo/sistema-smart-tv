9fbe383c7d06ee415b825e467231713796b7a679
Dev Lima <ndaraujolima@gmail.com>
2025-03-25 14:42:29 -0300

    Testes de Usuario

--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -2,7 +2,13 @@ public class Usuario {
     public static void main(String[] args) throws Exception {
         SmartTv smartTv = new SmartTv();
 
+        smartTv.ligar();
         System.out.println("Tv ligada ? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Tv ligada ? " + smartTv.ligada);
+
+        
         System.out.println("Canal atual: " + smartTv.canal);
         System.out.println("Volume atual: " + smartTv.volume);
     }
