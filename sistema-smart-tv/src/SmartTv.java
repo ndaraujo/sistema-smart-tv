01266a350c9cd5354879e6ea9989656cdb81804f
Dev Lima <ndaraujolima@gmail.com>
2025-03-25 14:59:05 -0300

    sysout implementados diretamente nos métodos

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -6,17 +6,21 @@ public class SmartTv {
 
     public void ligar() {
         ligada = true;
+        System.out.println("Tv ligada");
     }
 
     public void desligar() {
         ligada = false;
+        System.out.println("Tv desligada");
     }
 
     public void aumentarVolume() {
         volume++;
+        System.out.println("Volume: " + volume);
     }
 
     public void diminuirVolume() {
         volume--;
+        System.out.println("Volume: " + volume );
     }
 }
--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -3,17 +3,13 @@ public class Usuario {
         SmartTv smartTv = new SmartTv();
 
         smartTv.ligar();
-        System.out.println("Tv ligada ? " + smartTv.ligada);
 
         smartTv.desligar();
-        System.out.println("Tv ligada ? " + smartTv.ligada);
 
         smartTv.aumentarVolume();
-        System.out.println("Volume atual: " + smartTv.volume);
 
         smartTv.diminuirVolume();
-        System.out.println("Volume atual: " + smartTv.volume);
+        
 
-        System.out.println("Canal atual: " + smartTv.canal);
     }
 }
